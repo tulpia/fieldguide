249320e20ee4a461c7602651491a644dd0ebd9bb
Alec TEIHOTAATA <alec@myclientisrich.com>
2019-11-20 11:25:03 +0100

    Initialise le  projet

---
+++ pages/Items.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Items extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <p>Items</p>
+      </div>
+    );
+  }
+}
+
+export default Items;
