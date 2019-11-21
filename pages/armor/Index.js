64b1d5a0faddaaf26547e87bfc73abd045cd614e
Pierre "Kobaru" Tusseau <pierre.tusseau@gmail.com>
2019-11-21 11:34:17 +0100

    Remplace le composant en class par un composant fonctionnel page armures

--- pages/armor/Index.js
+++ pages/armor/Index.js
@@ -1,40 +1,25 @@
-import React, { Component } from "react";
-import fetch from "isomorphic-unfetch";
+import React, { Component, useState } from "react";
+// import Header from "../components/Header/Header";
 import Link from "next/link";
+import fetch from "isomorphic-unfetch";
 
-class Armors extends Component {
-  constructor(props) {
-    super(props)
+const Armors = props => {
+  const [armors, setArmors] = useState(props.armors)
+  const [value, setValue] = useState('')
+  const [ranks, setRanks] = useState({})
 
-    let ranks = {}
-    props.armors.forEach(armor => {
-      if (!(armor.rank in ranks)) {
-        console.log(armor.rank)
-        ranks = Object.assign({}, ranks, {[armor.rank]: false})
-      }
-    })
-    // Activer par défaut le High Rank
-    // for (var [rankName, rankValue] in ranks) {
-    //   if (rankName === 'high') {
-    //     rankValue = true
-    //   }
-    // }
-
-    this.state = {
-      value: '',
-      ranks: ranks,
-      armors: props.armors
+  armors.forEach(armor => {
+    if (!(armor.rank in ranks)) {
+      setRanks(Object.assign({}, ranks, {[armor.rank]: false}))
     }
+  })
 
-    console.log(this.state)
-  }
-
-  filterBySkill(event) {
+  const filterBySkill = event => {
     let value = ''
-    let newListOfArmors = this.props.armors
+    let newListOfArmors = props.armors
     if (event.target.value !== '') {
       value = event.target.value
-      newListOfArmors = this.props.armors.filter(armor => {
+      newListOfArmors = props.armors.filter(armor => {
         let hasArmorConcernedSkill = false
         if (armor.skills.length > 0) {
           armor.skills.forEach(skill => {
@@ -46,126 +31,121 @@ class Armors extends Component {
         return hasArmorConcernedSkill
       })
     }
-    this.setState({
-      value: value,
-      armors: newListOfArmors
-    })
+    setArmors(newListOfArmors)
+    setValue(value)
   }
 
-  render() {
-    const { skills } = this.props;
-    return (
-      <div>
-        <h1>MHW Armors</h1>
-        <section className="armors__filters">
-          <select className="filter__skill" onChange={this.filterBySkill.bind(this)} value={this.state.value}>
-            <option defaultValue value=''>----------------</option>
-            {skills.map(skill => {
-              return (
-                <option key={`skill_${skill.id}`} value={skill.id}>
-                  {skill.name}
-                </option>
-              )
-            })}
-          </select>
-          {(this.state.value !== '') ? (
-            <span> {this.state.armors.length} résultats</span>
-          ) : null}
-          <div>
-            <label htmlFor="low">
-              <input type="checkbox" id="low" name="low"/>
-              <span>Low Rank</span>
-            </label>
-            <label htmlFor="high">
-              <input type="checkbox" id="high" name="high" defaultChecked/>
-              <span>High Rank</span>
-            </label>
-            <label htmlFor="master">
-              <input type="checkbox" id="master" name="master"/>
-              <span>Master Rank</span>
-            </label>
-          </div>
-        </section>
-        <ul className="armors__list">
-          {this.state.armors.map(armor => {
+  return(
+    <div>
+      <h1>MHW Armors</h1>
+      <section className="armors__filters">
+        <select className="filter__skill" onChange={filterBySkill} value={value}>
+          <option defaultValue value=''>----------------</option>
+          {props.skills.map(skill => {
             return (
-              <li key={`armor_${armor.id}`} className={`list__armor armor--${armor.rank}`}>
-                <Link href="/armor/[id]" as={`/armor/${armor.id}`}>
-                  <a>
-                    {/* {armor.assets ? (
-                      <div className="armor__img-container">
-                        <img src={armor.assets.imageMale} />
-                      </div>
-                    ) : null } */}
-                    <div className="armor__infos">
-                      <span className="armor__id">
-                        [{armor.id}]
-                      </span>
-                      <span className="armor__name">
-                      {armor.name}
-                      </span>
-                    </div>
-                  </a>
-                </Link>
-              </li>
-            );
+              <option key={`skill_${skill.id}`} value={skill.id}>
+                {skill.name}
+              </option>
+            )
           })}
-        </ul>
-        <style jsx>{`
-          .armors__filters {
-            display: flex;
-          }
-          ul {
-            display: grid;
-            grid-template-columns: repeat(10, 1fr);
-            list-style-type: none;
-            gap: 10px;
-            padding: 0;
-          }
-          li {
-            display: block;
-            grid-column-end: span 1;
-          }
-          a {
-            height: 150px;
-            padding: 20px;
-            text-decoration: none;
-            color: black;
-            font-family: arial;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: space-between;
-          }
-          .armor__img-container {
-            height: 100px;
-            width: 100%;
-          }
-          .armor__img-container img {
-            object-fit: contain;
-            width: 100%; height: 100%;
-          }
-          .armor__id,
-          .armor__name {
-            display: block;
-            text-align: center;
-          }
-          .armor__id {
-            margin-bottom: 10px;
-          }
-          .armor--low {
-            background-color: #ffdbc5;
-          }
-          .armor--high {
-            background-color: #ff9d76;
-          }
-          .armor--master {
-            background-color: #ef4339;
-          }
-        `}</style>
-      </div>
-    );
-  }
+        </select>
+        {(value !== '') ? (
+          <span> {armors.length} résultats</span>
+        ) : null}
+        <div>
+          <label htmlFor="low">
+            <input type="checkbox" id="low" name="low"/>
+            <span>Low Rank</span>
+          </label>
+          <label htmlFor="high">
+            <input type="checkbox" id="high" name="high" defaultChecked/>
+            <span>High Rank</span>
+          </label>
+          <label htmlFor="master">
+            <input type="checkbox" id="master" name="master"/>
+            <span>Master Rank</span>
+          </label>
+        </div>
+      </section>
+      <ul className="armors__list">
+        {armors.map(armor => {
+          return (
+            <li key={`armor_${armor.id}`} className={`list__armor armor--${armor.rank}`}>
+              <Link href="/armor/[id]" as={`/armor/${armor.id}`}>
+                <a>
+                  {/* {armor.assets ? (
+                    <div className="armor__img-container">
+                      <img src={armor.assets.imageMale} />
+                    </div>
+                  ) : null } */}
+                  <div className="armor__infos">
+                    <span className="armor__id">
+                      [{armor.id}]
+                    </span>
+                    <span className="armor__name">
+                    {armor.name}
+                    </span>
+                  </div>
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      <style jsx>{`
+        .armors__filters {
+          display: flex;
+        }
+        ul {
+          display: grid;
+          grid-template-columns: repeat(10, 1fr);
+          list-style-type: none;
+          gap: 10px;
+          padding: 0;
+        }
+        li {
+          display: block;
+          grid-column-end: span 1;
+        }
+        a {
+          height: 150px;
+          padding: 20px;
+          text-decoration: none;
+          color: black;
+          font-family: arial;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: space-between;
+        }
+        .armor__img-container {
+          height: 100px;
+          width: 100%;
+        }
+        .armor__img-container img {
+          object-fit: contain;
+          width: 100%; height: 100%;
+        }
+        .armor__id,
+        .armor__name {
+          display: block;
+          text-align: center;
+        }
+        .armor__id {
+          margin-bottom: 10px;
+        }
+        .armor--low {
+          background-color: #ffdbc5;
+        }
+        .armor--high {
+          background-color: #ff9d76;
+        }
+        .armor--master {
+          background-color: #ef4339;
+        }
+      `}</style>
+    </div>
+  )
 }
 
 Armors.getInitialProps = async function() {
